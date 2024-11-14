063c7adeda00beb4a96bf6c22cba497c4ff243f7
Kauany Miranda <kauany.m.dantas@gmail.com>
2024-11-13 20:27:33 -0400

    Aula de Exceção

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using static System.Convert;
+using static System.Console;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Excecao c = new Excecao();
+            Write("Digite um valor: ");
+            double saldo = ToDouble(ReadLine());
+            c.SetSaldo(saldo);
+        }
+        catch (Exception Ex)
+        {
+            WriteLine($"Valor informado inválido {Ex.Message}");
+        }
+    }
+}
